9318ef8e1900ae634d139898dcc74ad21f303e7b
Dan Powell <dan@gobloom.io>
2024-04-02 17:31:10 -0600

    Swaps out default emoji

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,6 +23,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" style={{ "--navTop": "0px", "--fixedNavBottom": "0px" }}>
+      <link
+        rel="icon"
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>❤️</text></svg>"
+      />
       <body className={inter.className}>
         <Nav />
         {children}
